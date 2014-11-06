18097c4046bd103a3cec71d12a077f5cc52b8f0f
Sithu <sithukyaw007@gmail.com>
2014-11-06 08:57:08 +0800

    Retrieve comments from a post

--- server/api/comment/comment.controller.js
+++ server/api/comment/comment.controller.js
@@ -4,53 +4,96 @@ var _ = require('lodash');
 var Comment = require('./comment.model');
 var Feed = require('../feed/feed.model');
 var graph = require('fbgraph');
-var Q = require('q');
+var when = require('when');
+var syncFor=require('./syncFor');
 
+var options = {
+  pool:     { maxSockets:  Infinity } ,
+  headers:  { connection:  'keep-alive' }
+};
+
+var nextURL = '';
 
 // Get list of comments
+
+function finishQuery(){
+  console.log('query finished!!!!!!!!!!!!!!!!!!!!!!!');
+}
+
 exports.index = function(req, res) {
 
   Feed.find(function (err, feeds) {
     if(err) { return handleError(res, err); }
     // return res.json(200, feeds);
 
-    var options = {
-    pool:     { maxSockets:  Infinity } ,
-    headers:  { connection:  'keep-alive' }
-    };
-
-    // for(var i = 2; i >= 0; i--) {
-    //   console.log('feed id ' + feeds[i].id);
-    //   Q.nfcall(graph.setOptions(options).get(feeds[i].id + '/comments?fields=id,from,message,created_time,user_likes&limit=250')).then(function (res, cmts) {
-    //         console.log('comes into Q');
-    //        cmts.data.forEach(function(c) {
-    //         Comment.create(c, function(err, cmt) {
-    //           if(err) { return handleError(res, err); }
-    //         });
-    //     });
-    //     });
-    // }
-
-    for(var i = 2; i >= 0; i--) {
+    for(var i = feeds.length-1; i >= 0; i--) {
       console.log('feed id ' + feeds[i].id);
-     graph
-     .setOptions(options)
-        .get(feeds[i].id + '/comments?fields=id,from,message,created_time,user_likes&limit=250', function(err, cmts) {
-          // console.log(cmts.data.length);
-          cmts.data.forEach(function(c) {
-            Comment.create(c, function(err, cmt) {
-              if(err) { return handleError(res, err); }
-            });
-          });
-        });
+
+      var reqURL = feeds[i].id + '/comments?fields=id,from,message,created_time,user_likes&summary=1';
+      // var reqURL =  '6815841748_10151986355271749/comments?fields=id,from,message,created_time,user_likes&summary=1';
+
+      graph.setOptions(options).get(reqURL, function(err, cmts) {
+         if(err) { console.log('error in first graph call'); }
+           syncFor(1,cmts.summary.total_count,"start",function(i,status,call){
+               if(status === "done")
+                console.log("array iteration is done")
+              else {
+                  if(nextURL === '') {
+                    console.log('comes to normal URL');
+                    getComments(reqURL,function(){
+                       call('next') // this acts as increment (i++)
+                     })
+                  } else {
+                      console.log('comes into next URL');
+                      if (nextURL !== 'no_next_url') {
+                            getComments(nextURL,function(){
+                           call('next') // this acts as increment (i++)
+                         })
+                      } else {
+                          call('next');
+                      }
+                  }
+              }
+            })
+      });
     }
     return res.json(200,  'done');
   });
-
-
-
-
 };
+
+var count = 1;
+function getComments(reqURL, callback) {
+  var promise = when.promise(function(resolve, reject, notify) {
+      graph.setOptions(options).get(reqURL, function(err, cmts) {
+         if(err) { reject(err) }
+          resolve(cmts)
+      });
+  });
+
+  promise.then(function(cmts){
+    console.log(count++ + '.) Comes into promise. comments length is ' + cmts.data.length);
+    cmts.data.forEach(function(c) {
+      Comment.create(c, function(err, cmt) {
+        if(err) { console.log('error in creating comment'); }
+      });
+    });
+    if(cmts.paging.next !== undefined) {
+          console.log('there is next content');
+          nextURL = cmts.paging.next;
+    } else {
+      console.log('there comes to else blockkkkkk');
+      console.log(JSON.stringify(cmts.paging));
+      nextURL = 'no_next_url';
+    }
+    callback();
+  });
+
+  promise.catch(function(err){
+    console.log('error in promise catch');
+    console.log(err);
+  });
+}
+
 
 // Get a single comment
 exports.show = function(req, res) {
